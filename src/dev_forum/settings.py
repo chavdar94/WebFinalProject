bf95cad830247a0e2aa91b751835bbf0d04051a4
Chavdar <chokobg@gmail.com>
2023-05-18 21:51:10 +0300

    init

--- src/dev_forum/settings.py
+++ src/dev_forum/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "web_final",
         "USER": "postgres",
-        "PASSWORD": "kokosan1",
+        "PASSWORD": "mypass",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
