bac6edb9cffd93a246ca611c1c722b3c6890b101
chavdar94 <chokobg@gmail.com>
2023-06-30 10:04:20 +0300

    Added responsive styling and made added permissions to add new topics only for moderators and admins

--- src/apps/forum/mixins/moderator_group_mixin.py
+++ src/apps/forum/mixins/moderator_group_mixin.py
@@ -4,8 +4,9 @@ from django.http import Http404
 
 class GroupRequiredMixin(AccessMixin):
     allowed_groups = []
+
     def dispatch(self, request, *args, **kwargs):
         user_groups = request.user.groups.values_list('name', flat=True)
-        if not set(self.allowed_groups) and not set(user_groups):
+        if not set(self.allowed_groups) or not set(user_groups):
             raise Http404()
         return super().dispatch(request, *args, **kwargs)
