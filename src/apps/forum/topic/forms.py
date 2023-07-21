899346e2c0e06e679c62f9e0efaeca9c2d73e7eb
chavdar94 <chokobg@gmail.com>
2023-07-21 14:09:58 +0300

    added styles for top create

--- src/apps/forum/topic/forms.py
+++ src/apps/forum/topic/forms.py
@@ -7,3 +7,11 @@ class TopicCreateForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ('name',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_fields_classes()
+
+    def __set_fields_classes(self):
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'rounded-xl w-3/4 bg-slate-300 text-lg font-["Poppins"]'
