b993e6c800ecfe6064c9f77e4293945a73df0152
chavdar94 <chokobg@gmail.com>
2023-07-21 20:08:09 +0300

    added middleware to restrict access to /admin/ ulr

--- src/apps/core/middleware.py
+++ src/apps/core/middleware.py
@@ -6,9 +6,13 @@ class NoDjangoAdminForEndUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request):
+    def _middleware(self, request, *args, **kwargs):
         if request.path.startswith("/admin/"):
-            if not request.user.is_authenticated and not request.user.is_staff:
+            if not request.user.is_authenticated or not request.user.is_staff:
                 raise Http404()
         response = self.get_response(request)
         return response
+
+    def __call__(self, request, *args, **kwargs):
+        self._middleware(request, *args, **kwargs)
+        return self.get_response(request, *args, **kwargs)
